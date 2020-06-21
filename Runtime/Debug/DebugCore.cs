fb675bd4621d298bae14a45dcf5f54d351ce2a65
Lisandro Crespo <hey@lisandroct.com>
2020-06-21 20:18:15 -0300

    Add Debug

---
+++ Runtime/Debug/DebugCore.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using lisandroct.Core.Utils;
+
+namespace lisandroct.Core.Debug
+{
+    public static class DebugCore
+    {
+        public static void DrawRect(Rect rect) => DrawRect(rect, Color.white);
+
+        public static void DrawRect(Rect rect, Color color) {
+            Vector3 a = new Vector2(rect.xMin, rect.yMin).sX0Y();
+            Vector3 b = new Vector2(rect.xMax, rect.yMin).sX0Y();
+            Vector3 c = new Vector2(rect.xMax, rect.yMax).sX0Y();
+            Vector3 d = new Vector2(rect.xMin, rect.yMax).sX0Y();
+
+            UnityEngine.Debug.DrawLine(a, b, color);
+            UnityEngine.Debug.DrawLine(b, c, color);
+            UnityEngine.Debug.DrawLine(c, d, color);
+            UnityEngine.Debug.DrawLine(d, a, color);
+        }
+    }
+}
