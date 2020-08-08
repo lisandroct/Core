3c1aaabce8e5a4a2b9ded1223fe5134653f25d15
Lisandro Crespo <hey@lisandroct.com>
2020-08-08 17:26:01 -0300

    Add core editor folders

---
+++ Editor/CoreFolders.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace lisandroct.Core.Editor
+{
+    public static class CoreFolders
+    {
+        private const string Assets = "Assets";
+        private const string Core = "lisandroct";
+        private const string Editor = "Editor";
+        private const string Resources = "Resources";
+
+        public static string CorePath => $"{Assets}/{Core}";
+        public static string SettingsPath => $"{CorePath}/{Editor}/{Resources}";
+
+        public static void Create()
+        {
+            if (!AssetDatabase.IsValidFolder(CorePath))
+            {
+                AssetDatabase.CreateFolder(Assets, Core);
+            }
+            if (!AssetDatabase.IsValidFolder(SettingsPath))
+            {
+                AssetDatabase.CreateFolder(CorePath, Editor);
+                AssetDatabase.CreateFolder($"{CorePath}/{Editor}", Resources);
+            }
+        }
+    }
+}
---
+++ Editor/FriendlyTypeName.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Text;
+
+public static class FriendlyTypeName
+{
+    public static string GetFriendlyName(this Type type)
+    {
+        if (type.IsPrimitive)
+        {
+            if (type == typeof(bool))
+            {
+                return "Bool";
+            }
+
+            if (type == typeof(float))
+            {
+                return "Float";
+            }
+
+            if (type == typeof(int))
+            {
+                return "Int";
+            }
+
+            if (type == typeof(uint))
+            {
+                return "UInt";
+            }
+
+            if (type == typeof(long))
+            {
+                return "Long";
+            }
+
+            if (type == typeof(ulong))
+            {
+                return "ULong";
+            }
+
+            if (type == typeof(short))
+            {
+                return "Short";
+            }
+
+            if (type == typeof(ushort))
+            {
+                return "UShort";
+            }
+        }
+
+        if (type.IsArray)
+        {
+            return $"{GetFriendlyName(type.GetElementType())}Array";
+        }
+
+        if (type.IsConstructedGenericType)
+        {
+            var builder = new StringBuilder();
+            foreach (var generic in type.GenericTypeArguments)
+            {
+                builder.Append(GetFriendlyName(generic));
+            }
+
+            builder.Append(type.Name.Split('`')[0]);
+
+            return builder.ToString();
+        }
+
+        return type.Name;
+    }
+}
